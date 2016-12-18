e91b1b33bc060538bad3ede9c9d2fe042e48e5e3
Matthew Del Salto <mdelsalto@blackboxusa.com>
2016-12-18 18:12:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# flacup
+#modified by nwcd
+#added functionality to check between 16/24bit
+#added functionality to run in batch as a shell script
+#changed folder renaming to match nwcd guidelines
+
+flacup WILL rename your folders with the corresponding match, if it doesn't match press B to abort.
+
+#Setup instructions
+Install autotunes:
+
+Install Python 3.
+Install Metaflac.
+Install mktorrent
+Install the latest version of mktorrent from https://github.com/Rudde/mktorrent.
+Install the dependencies with sudo pip3 install -r requirements.txt. 
+
+Open and modify the username and path variables in flacup.sh
+Set passwords in autotunes24 and autotunes16 by searching for REPLACEME string
+
+To Do:
+#
+    Dupe-checking is not yet implemented.
+    Only works with 24bit or 16bit FLAC for now.
+    flacup is provided for demonstration purposes only.
+#
---
+++ flacup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#notwhat dev dec 18 2016
+#Finds directories with flac and attempt to split 16bit and 24bit based on filesize of directory
+#Root directory of your music library without trailing slash
+BASEDIR=/mnt/music/24flac
+#Gazelle site username
+USER=satisflaction
+find $BASEDIR -maxdepth 2 -type f -name '*.flac*' -printf '%h\0' | sort -zu | sed -z 's/$/\n/' > flacout.tmp
+ALBUM_DIRS="$line"
+
+#grabs system info
+while IFS='' read -r line || [[ -n "$line" ]]; do
+
+#get space
+du -sh "$line"
+
+
+getbitrate()
+{
+#gets the directory base folder name to look for 24bit strings
+baseline=`basename "$line"`
+
+flacfile=`find "$line" -name "*.flac" | head -n 1`
+BITRATE=`metaflac --show-bps "$flacfile"`
+
+#if [[ $baseline == *"24"* ]]
+#then
+#NEWBITRATE="24bit"
+#echo "Setting bitrate to 24bit/96khz";
+#BITRATE=$NEWBITRATE
+#If we cannot determine ask the user
+#else
+#echo "borked"
+#askbitrate
+#fi
+
+
+}
+
+askbitrate(){
+echo "Is this release 24bit? y/n:"
+read BITRATE </dev/tty
+if [ $BITRATE = "y" ];
+then
+NEWBITRATE="24bit"
+echo "Setting bitrate to 24bit/96khz"
+BITRATE=$NEWBITRATE
+else
+NEWBITRATE="16bit"
+echo "Setting bitrate to 16bit/44khz"
+BITRATE=$NEWBITRATE
+fi
+}
+
+getbitrate
+
+echo "Reported as $BITRATE bit - Operation #flacup"
+sudo python3 ./autotunes$BITRATE -o . $USER "$line" </dev/tty
+
+
+done < flacout.tmp
